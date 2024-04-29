9ec45c4464f5873dd0faead62584f025e26231f7
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2024-04-29 03:43:45 -0500

    feat: :sparkles: Setup astro template

---
+++ astro.config.ts
@@ -0,0 +1,76 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
+import fs from "fs";
+import rehypeExternalLinks from "rehype-external-links";
+import remarkUnwrapImages from "remark-unwrap-images";
+
+import { expressiveCodeOptions } from "./src/site.config";
+import { remarkReadingTime } from "./src/utils/remark-reading-time";
+
+// https://astro.build/config
+export default defineConfig({
+	image: {
+		domains: ["oscargicast.com", "credly.com"],
+		remotePatterns: [{
+			hostname: '**.amazonaws.com',
+			protocol: 'https',
+		}],
+	},
+	integrations: [
+		expressiveCode(expressiveCodeOptions),
+		icon(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		mdx(),
+	],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					rel: ["nofollow, noopener, noreferrer"],
+					target: "_blank",
+				},
+			],
+		],
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		remarkRehype: {
+			footnoteLabelProperties: {
+				className: [""],
+			},
+		},
+	},
+	// https://docs.astro.build/en/guides/prefetch/
+	prefetch: true,
+	// ! Please remember to replace the following site property with your own domain
+	site: "https://oscargicast.com",
+	vite: {
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+		plugins: [rawFonts([".ttf", ".woff"])],
+	},
+});
+
+function rawFonts(ext: string[]) {
+	return {
+		name: "vite-plugin-raw-fonts",
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-expect-error:next-line
+		transform(_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
+}
